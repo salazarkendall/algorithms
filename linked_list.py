993b7f0e25585cf38884a2cb149759285fac63ef
salazarkendall <kendallsalazarvargas@gmail.com>
2024-04-30 11:34:13 -0600

    added: print functions and LinkedList.append function

--- linked_list.py
+++ linked_list.py
@@ -4,9 +4,34 @@ class Node:
     self.next = None
 
   def __str__(self) -> str:
-    return f'Node value {self.value}'
+    return f'Node value: {self.value}'
 
 class LinkedList:
   def __init__(self, value):
     new_node = Node(value)
     self.head = self.tail = new_node
+    self.length = 1
+
+  def append(self, value):
+    new_node = Node(value)
+    self.tail.next = new_node
+    self.tail = new_node
+
+  def __str__(self) -> str:
+    result = '['
+    temp = self.head
+    while temp:
+      result += f'{temp.value}'
+      temp = temp.next
+      if temp:
+        result += ', '
+    result += ']'
+    return result
+
+# TESTING
+my_list = LinkedList(0)
+
+for i in range(5):
+  my_list.append(i)
+
+print(my_list)
